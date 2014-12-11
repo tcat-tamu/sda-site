fa05123df316e5e6b8417a3738ee44d0fca0a0c3
Matthew Barry <mbarry@tamu.edu>
2014-12-10 18:50:44 -0600

    added Symfony PHP application to handle email subscriber management
    https://bugs.citd.tamu.edu/show_bug.cgi?id=* master

---
+++ main/edu.tamu.tcat.sda.site.emailapp/src/SDA/SubscriptionBundle/Form/SubscriberType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace SDA\SubscriptionBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class SubscriberType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('entered')
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'SDA\SubscriptionBundle\Entity\Subscriber',
+            'csrf_protection' => false,
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'subscriber';
+    }
+}
---
+++ main/edu.tamu.tcat.sda.site.emailapp/src/SDA/SubscriptionBundle/SDASubscriptionBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SDA\SubscriptionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SDASubscriptionBundle extends Bundle
+{
+}
