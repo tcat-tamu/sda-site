5d1c83b14c443ce131068fe038bfa98fc208a985
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2015-05-08 18:09:05 -0500

    Commented out library page features from main.js

--- main/edu.tamu.tcat.sda.site.web/web/scripts/main.js
+++ main/edu.tamu.tcat.sda.site.web/web/scripts/main.js
@@ -10,45 +10,45 @@ define(function (require) {
    require('trc-entries-biblio');
    require('modules/library/search');
 
-   var Router = Marionette.AppRouter.extend({
-      appRoutes: {
-         ":id": 'displayBiblioWork'
-      }
-   });
-
-   var Controller = Marionette.Controller.extend({
-
-      initialize: function(opts) {
-         this.mergeOptions(opts, ['region']);
-      },
-
-      displayBiblioWork: function(id) {
-         this.region.show(new BookreaderView({
-            htid: id
-         }));
-      }
-
-
-   });
-
-   var Layout = Marionette.LayoutView.extend({
-      regions: {
-         "bookreader": '#bookreader'
-      }
-   });
-
-   var layout = new Layout({
-      el: '#page_body'
-   });
-
-
-
-   var router = new Router({
-      controller: new Controller({
-         region: layout.getRegion('bookreader')
-      })
-   });
-
-   Backbone.history.start();
+   // var Router = Marionette.AppRouter.extend({
+   //    appRoutes: {
+   //       ":id": 'displayBiblioWork'
+   //    }
+   // });
+   //
+   // var Controller = Marionette.Controller.extend({
+   //
+   //    initialize: function(opts) {
+   //       this.mergeOptions(opts, ['region']);
+   //    },
+   //
+   //    displayBiblioWork: function(id) {
+   //       this.region.show(new BookreaderView({
+   //          htid: id
+   //       }));
+   //    }
+   //
+   //
+   // });
+   //
+   // var Layout = Marionette.LayoutView.extend({
+   //    regions: {
+   //       "bookreader": '#bookreader'
+   //    }
+   // });
+   //
+   // var layout = new Layout({
+   //    el: '#page_body'
+   // });
+   //
+   //
+   //
+   // var router = new Router({
+   //    controller: new Controller({
+   //       region: layout.getRegion('bookreader')
+   //    })
+   // });
+   //
+   // Backbone.history.start();
 
 });
