92e97d5f68606c4d8717fd08ab7b613da3ce31fa
Matthew Barry <mbarry@tamu.edu>
2015-07-14 11:20:43 -0500

    refactor author views into separate module
    https://bugs.citd.tamu.edu/show_bug.cgi?id=* master

--- main/edu.tamu.tcat.sda.site.web/reader_app/src/js/work_info/views/work_display_view.js
+++ main/edu.tamu.tcat.sda.site.web/reader_app/src/js/work_info/views/work_display_view.js
@@ -4,6 +4,8 @@ define(function (require) {
    var nunjucks = require('nunjucks');
    var _ = require('underscore');
 
+   var AuthorsView = require('./authors_view');
+
 
    var REGEX_STRIP_TAGS = /(<([^>]+)>)/ig;
 
@@ -11,57 +13,6 @@ define(function (require) {
 
    env.addFilter('striptags', function (str) {
       return str.replace(REGEX_STRIP_TAGS, '');
-   });
-
-
-   var AuthorView = Marionette.ItemView.extend({
-      template: function (ctx) {
-         var fname = ctx.firstName ? ctx.firstName : '';
-         var lname = ctx.lastName ? ctx.lastName : '';
-         return fname + ' ' + lname + ' ';
-      },
-
-      tagName: 'a',
-
-      className: 'author',
-
-      events: {
-         click: function (evt) {
-            evt.preventDefault();
-            var authorId = this.model.get('authorId');
-
-            if (authorId) {
-               this.channel.trigger('author:show', authorId);
-            }
-         }
-      },
-
-      initialize: function (options) {
-         this.mergeOptions(options, ['channel']);
-      },
-
-      onShow: function () {
-         if (!this.model.get('authorId')) {
-            this.$el.addClass('disabled');
-         }
-      }
-   });
-
-
-   var AuthorsView = Marionette.CollectionView.extend({
-      tagName: 'span',
-      className: 'authors',
-      childView: AuthorView,
-
-      childViewOptions: function () {
-         return {
-            channel: this.channel
-         };
-      },
-
-      initialize: function (options) {
-         this.mergeOptions(options, ['channel']);
-      }
    });
 
 
