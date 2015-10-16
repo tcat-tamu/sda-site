45c43c1747f5eeba7b888e67d408384e66c8aaf2
Matthew Barry <mbarry@tamu.edu>
2015-10-16 15:49:43 -0500

    stub out home page angular app with yeoman

---
+++ main/edu.tamu.tcat.sda.site.web/home_app/src/app/index.module.js
@@ -0,0 +1,7 @@
+(function () {
+   'use strict';
+
+   angular
+      .module('sda.site', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router']);
+
+})();
---
+++ main/edu.tamu.tcat.sda.site.web/home_app/src/app/main/main.controller.js
@@ -0,0 +1,18 @@
+(function () {
+   'use strict';
+
+   angular
+      .module('sda.site')
+      .controller('MainController', MainController);
+
+   /** @ngInject */
+   function MainController() {
+      var vm = this;
+
+      activate();
+
+      function activate() {
+      }
+   }
+
+})();
