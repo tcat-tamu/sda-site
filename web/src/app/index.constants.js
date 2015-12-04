8883bdaf2c8fffef22055b22ed64f55c7a967338
Matthew Barry <mbarry@tamu.edu>
2015-12-04 15:49:49 -0600

    install citeproc-js CSL bibliography tool

--- web/src/app/index.constants.js
+++ web/src/app/index.constants.js
@@ -1,10 +1,11 @@
-/* global _:false config:false */
+/* global _:false config:false CSL:false */
 (function () {
    'use strict';
 
    angular
       .module('sda')
       .constant('config', config)
-      .constant('_', _);
+      .constant('_', _)
+      .constant('CSL', CSL);
 
 })();
