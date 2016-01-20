139122bc9a685ebcba2ac418ed58657ef298bb33
Matthew Barry <mbarry@tamu.edu>
2016-01-20 15:50:40 -0600

    add missing about controller

---
+++ web/src/app/about/about.controller.js
@@ -0,0 +1,12 @@
+(function () {
+   'use strict';
+
+   angular
+      .module('sda')
+      .controller('AboutController', AboutController);
+
+   /** @ngInject */
+   function AboutController() {
+   }
+
+})();
