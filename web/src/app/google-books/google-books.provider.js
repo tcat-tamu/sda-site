62c7c911d60193ea2448b3bfd562e70ac68f93e7
Matthew Barry <mbarry@tamu.edu>
2016-01-21 19:26:14 -0600

    add citations for inspiration

--- web/src/app/google-books/google-books.provider.js
+++ web/src/app/google-books/google-books.provider.js
@@ -1,3 +1,6 @@
+/**
+ * Inspired by https://github.com/angular-ui/angular-google-maps/blob/f7c17e4336344afe9c31ca8b8a1e200e268f4979/src/coffee/providers/map-loader.coffee
+ */
 (function () {
    'use strict';
 
