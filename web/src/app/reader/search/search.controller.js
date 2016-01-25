4f9c36677f93847ef666987c05701ece6a01a362
Matthew Barry <mbarry@tamu.edu>
2016-01-25 17:46:12 -0600

    improve styling on article search results; display more info

--- web/src/app/reader/search/search.controller.js
+++ web/src/app/reader/search/search.controller.js
@@ -6,10 +6,14 @@
       .controller('ReaderSearchController', ReaderSearchController);
 
    /** @ngInject */
-   function ReaderSearchController($stateParams, $scope, articleRepository) {
+   function ReaderSearchController($stateParams, $scope, articleRepository, $http) {
       var vm = this;
 
       vm.queryResult = {};
+      vm.displayFirstPage = displayFirstPage;
+      vm.displayPrevPage = displayPrevPage;
+      vm.displayNextPage = displayNextPage;
+      vm.displayLastPage = displayLastPage;
 
       activate();
 
@@ -19,6 +23,42 @@
          if (vm.query) {
             $scope.$emit('set:query', vm.query);
             vm.queryResult = articleRepository.search({ q: vm.query });
+         }
+      }
+
+      function displayLastPage() {
+         if (vm.queryResult.last) {
+            $http.get(vm.queryResult.query.last.uri)
+               .then(function (resp) {
+                  vm.queryResult = resp.data;
+               });
+         }
+      }
+
+      function displayNextPage() {
+         if (vm.queryResult.next) {
+            $http.get(vm.queryResult.query.next.uri)
+               .then(function (resp) {
+                  vm.queryResult = resp.data;
+               });
+         }
+      }
+
+      function displayPrevPage() {
+         if (vm.queryResult.previous) {
+            $http.get(vm.queryResult.query.previous.uri)
+               .then(function (resp) {
+                  vm.queryResult = resp.data;
+               });
+         }
+      }
+
+      function displayFirstPage() {
+         if (vm.queryResult.first) {
+            $http.get(vm.queryResult.query.first.uri)
+               .then(function (resp) {
+                  vm.queryResult = resp.data;
+               });
          }
       }
    }
