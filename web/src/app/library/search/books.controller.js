8d40b8cd47eea3c91f088ae53fc44b93f79fbac3
Matthew Barry <mbarry@tamu.edu>
2016-01-29 17:13:49 -0600

    add throbber to results loading, but wait 250ms to display so it doesn't flash for fast-loading

--- web/src/app/library/search/books.controller.js
+++ web/src/app/library/search/books.controller.js
@@ -6,12 +6,15 @@
       .controller('LibraryBooksSearchController', LibraryBooksSearchController);
 
    /** @ngInject */
-   function LibraryBooksSearchController($stateParams, $state, $scope, workRepository) {
+   function LibraryBooksSearchController($stateParams, $state, $scope, $timeout, workRepository) {
       var vm = this;
 
       vm.books = [];
       vm.query = '';
       vm.currentId = null;
+      vm.loading = true;
+      vm.showThrobber = false;
+      vm.throbberTimeout = null;
 
       activate();
 
@@ -20,9 +23,22 @@
          vm.currentId = $stateParams.id;
          $scope.$emit('set:query:book', vm.query);
          vm.books = workRepository.query({ q: vm.query }, onResultsLoaded);
+
+         // prevent "flash" on quick load
+         vm.loadingTimeout = $timeout(function () {
+            vm.showThrobber = true;
+         }, 250);
       }
 
       function onResultsLoaded() {
+         if (vm.loadingTimeout) {
+            $timeout.cancel(vm.loadingTimeout);
+            vm.loadingTimeout = null;
+         }
+
+         vm.showThrobber = false;
+         vm.loading = false;
+
          if (!vm.currentId && vm.books.length > 0) {
             $state.go('sda.library.search-books', { query: vm.query, id: vm.books[0].id })
          }
--- web/src/app/library/search/people.controller.js
+++ web/src/app/library/search/people.controller.js
@@ -6,12 +6,15 @@
       .controller('LibraryPeopleSearchController', LibraryPeopleSearchController);
 
    /** @ngInject */
-   function LibraryPeopleSearchController($stateParams, $state, $scope, personRepository) {
+   function LibraryPeopleSearchController($stateParams, $state, $scope, $timeout, personRepository) {
       var vm = this;
 
       vm.people = [];
       vm.query = '';
       vm.currentId = null;
+      vm.loading = true;
+      vm.loadingTimeout = null;
+      vm.showThrobber = false;
 
       activate();
 
@@ -20,9 +23,22 @@
          vm.currentId = $stateParams.id;
          $scope.$emit('set:query:people', vm.query);
          vm.people = personRepository.query({ syntheticName: vm.query }, onResultsLoaded);
+
+         // prevent "flash" on quick load
+         vm.loadingTimeout = $timeout(function () {
+            vm.showThrobber = true;
+         }, 250);
       }
 
       function onResultsLoaded() {
+         if (vm.loadingTimeout) {
+            $timeout.cancel(vm.loadingTimeout);
+            vm.loadingTimeout = null;
+         }
+
+         vm.showThrobber = false;
+         vm.loading = false;
+
          if (!vm.currentId && vm.people.length > 0) {
             $state.go('sda.library.search-people', { query: vm.query, id: vm.people[0].id })
          }
