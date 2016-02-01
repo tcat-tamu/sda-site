1ef01350d323ccb549cec81f0736d41c198d554a
Matthew Barry <mbarry@tamu.edu>
2016-02-01 17:03:51 -0600

    show throbber when loading people on library page

--- web/src/app/library/people/person.controller.js
+++ web/src/app/library/people/person.controller.js
@@ -6,11 +6,13 @@
       .controller('LibraryPersonController', LibraryPersonController);
 
    /** @ngInject */
-   function LibraryPersonController($stateParams, $scope, personRepository, workRepository) {
+   function LibraryPersonController($stateParams, $scope, $timeout, personRepository, workRepository) {
       var vm = this;
 
       vm.person = null;
       vm.relatedWorks = [];
+      vm.loading = true;
+      vm.showThrobber = false;
 
       activate();
 
@@ -19,10 +21,29 @@
 
          $scope.$emit('set:query:people', null);
 
+         vm.loadingTimeout = $timeout(function () {
+            vm.showThrobber = true;
+            vm.loadingTimeout = null;
+         }, 250);
+
          if (id) {
-            vm.person = personRepository.get({ id: id });
+            vm.person = personRepository.get({ id: id }, onPersonLoaded);
+
             vm.relatedWorks = workRepository.query({ aid: id });
          }
+      }
+
+      function onPersonLoaded() {
+         hideThrobber();
+      }
+
+      function hideThrobber() {
+         if (vm.loadingTimeout) {
+            $timeout.cancel(vm.loadingTimeout);
+            vm.loadingTimeout = null;
+         }
+         vm.loading = false;
+         vm.showThrobber = false;
       }
    }
 
