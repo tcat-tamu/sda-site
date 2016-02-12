dccc74e2d0632dac3f07f597713a524aa8318512
Matthew Barry <mbarry@tamu.edu>
2016-02-12 13:12:57 -0600

    update (and embed) mock API data

--- src/app/reader/components/article-repo/article-repo.service.js
+++ src/app/reader/components/article-repo/article-repo.service.js
@@ -6,7 +6,24 @@
       .factory('articleRepository', articleRepositoryFactory);
 
    /** @ngInject */
-   function articleRepositoryFactory($resource, config) {
+   function articleRepositoryFactory($resource, config, articleMock, $timeout) {
+
+      // HACK: mock data retrieval for demo
+      return {
+         get: function (opts, cb) {
+            opts = opts || {};
+            cb = cb || function () {};
+
+            if (opts.id) {
+               $timeout(function () { cb(articleMock); });
+               return articleMock
+            } else {
+               return null;
+            }
+         }
+      }
+
+
       var uri = config.apiEndpoint + '/articles/:id';
 
       var defaultParameters = {};
