8b864fe0fd66da20363b189394324137948168f0
Matthew Barry <mbarry@tamu.edu>
2016-10-26 13:59:15 -0500

    use cubic easing for more natural animation

--- apps/library/src/app/components/slide-toggle/slide.animation.js
+++ apps/library/src/app/components/slide-toggle/slide.animation.js
@@ -1,9 +1,16 @@
+/* global jQuery: true */
 (function () {
   'use strict';
 
   angular
     .module('sdaLibrary')
     .animation('.slide', slideToggle);
+
+  // easing fn from http://gsgd.co.uk/sandbox/jquery/easing/
+  jQuery.easing.easeInOutCubic = function (x, t, b, c, d) {
+    if ((t/=d/2) < 1) return c/2*t*t*t + b;
+    return c/2*((t-=2)*t*t + 2) + b;
+  };
 
   function slideToggle() {
     var NG_HIDE_CLASS = 'ng-hide';
@@ -15,13 +22,13 @@
 
     function beforeAddClass(element, className, done) {
       if (className === NG_HIDE_CLASS) {
-        element.slideUp(done);
+        element.slideUp(250, 'easeInOutCubic', done);
       }
     }
 
     function removeClass(element, className, done) {
       if (className === NG_HIDE_CLASS) {
-        element.hide().slideDown(done);
+        element.hide().slideDown(250, 'easeInOutCubic', done);
       }
     }
   }
