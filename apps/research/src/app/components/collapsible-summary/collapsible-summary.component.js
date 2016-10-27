6aa872b1866d825acde36b9c89f19acefc5c4cbc
Matthew Barry <mbarry@tamu.edu>
2016-10-27 10:40:00 -0500

    collapsible summary component for reuse

---
+++ apps/research/src/app/components/collapsible-summary/collapsible-summary.component.js
@@ -0,0 +1,29 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('sdaLibrary')
+    .component('collapsibleSummary', {
+      templateUrl: 'app/components/collapsible-summary/collapsible-summary.html',
+      bindings: {
+        content: '<'
+      },
+      controller: CollapsibleSummaryController
+    });
+
+  var CHAR_THRESHOLD = 500;
+
+  /** @ngInject */
+  function CollapsibleSummaryController($scope) {
+    var vm = this;
+
+    activate();
+
+    function activate() {
+      $scope.$watch('$ctrl.content', function (newContent) {
+        vm.longEnough = (!newContent || newContent.length > CHAR_THRESHOLD);
+      });
+    }
+  }
+
+})();
