a128998ce5012bc0671fa62c3ee9c4d371c8dbbc
Matthew Barry <mbarry@tamu.edu>
2016-11-10 12:34:39 -0600

    add see also component to shared SDA components

--- apps/sda/src/index.js
+++ apps/sda/src/index.js
@@ -3,11 +3,14 @@ var angular = require('angular');
 var sitenavProviderService = require('./sda-sitenav/sda-sitenav.service');
 var sitenavComponent = require('./sda-sitenav/sda-sitenav.component');
 var headerComponent = require('./sda-header/sda-header.component');
+var seeAlsoComponent = require('./see-also/see-also.component');
 
 angular
   .module('sda', [
-    'ngMaterial'
+    'ngMaterial',
+    'trcSeeAlso'
   ])
   .provider('sdaSitenav', sitenavProviderService.SdaSitenavProvider)
   .component('sdaSitenav', sitenavComponent.component)
-  .component('sdaHeader', headerComponent.component);
+  .component('sdaHeader', headerComponent.component)
+  .component('seeAlso', seeAlsoComponent);
