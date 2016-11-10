f4a1d6a6de20363fa602e253c87077851fd6b876
Matthew Barry <mbarry@tamu.edu>
2016-11-10 17:57:28 -0600

    update build script to pre-build shared SDA lib

--- build.sh
+++ build.sh
@@ -3,12 +3,23 @@
 gulp_build(){
   cd "apps/$1"
   npm install
+
+  # remove local sda module install (in case it has been updated)
+  if [ -d "bower_components/sda" ]; then
+    rm -rf "bower_components/sda"
+  fi
+
   bower install
   gulp
   cd ../..
+}
+
+copy_app(){
   rm -rf "web/$1"
   cp -r "apps/$1/dist" "web/$1"
 }
+
+gulp_build sda
 
 APPS=(admin book-reader influence-map research themes vwise)
 
@@ -17,6 +28,10 @@ for APP in "${APPS[@]}"; do
 done
 
 wait
+
+for APP in "${APPS[@]}"; do
+  copy_app "$APP"
+done
 
 cd web
 bundle install
