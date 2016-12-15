487c41881fda223923f580e6279973b05c20fcdc
Matthew Barry <mbarry@tamu.edu>
2016-12-14 18:26:47 -0600

    refactor auth cred persistence to TRC auth service

--- apps/admin/src/app/index.run.js
+++ apps/admin/src/app/index.run.js
@@ -5,30 +5,8 @@
     .module('sdaAdmin')
     .run(runBlock);
 
-  var AUTH_PERSIST_KEY = 'sdaAuthCredentials';
-
   /** @ngInject */
-  function runBlock($log, $window, trcAuth) {
-
-    trcAuth.onLogin(function (creds) {
-      $log.debug('logged in as', creds.accountId);
-      $window.localStorage.setItem(AUTH_PERSIST_KEY, angular.toJson(creds));
-    });
-
-    trcAuth.onLogout(function () {
-      $log.debug('logged out');
-      $window.localStorage.removeItem(AUTH_PERSIST_KEY);
-    });
-
-    // attempt to load persisted authentication credentials
-    var jsonCreds = $window.localStorage.getItem(AUTH_PERSIST_KEY);
-    if (jsonCreds) {
-      $log.debug('loading credentials from localStorage');
-      var creds = JSON.parse(jsonCreds);
-      creds.expires = new Date(creds.expires);
-      trcAuth.setCredentials(creds);
-    }
-
+  function runBlock($log, trcAuth) {
     if (!trcAuth.isAuthenticated()) {
       trcAuth.loginGuest();
     }
