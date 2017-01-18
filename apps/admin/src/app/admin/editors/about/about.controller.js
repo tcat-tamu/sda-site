24bd59259da7c71115bad9da9ce215453f7cc388
Matthew Barry <mbarry@tamu.edu>
2017-01-18 17:23:45 -0600

    use click callback on article-type-search component; remove add button from about admin (only way to create is through categories)

--- apps/admin/src/app/admin/editors/about/about.controller.js
+++ apps/admin/src/app/admin/editors/about/about.controller.js
@@ -6,44 +6,15 @@
     .controller('AboutEditorSectionController', AboutEditorSectionController);
 
   /** @ngInject */
-  function AboutEditorSectionController($state, $mdDialog, sdaToast, articlesRepo) {
+  function AboutEditorSectionController($state) {
     var vm = this;
 
-    vm.createArticle = createArticle;
+    vm.editAboutPage = editAboutPage;
 
     // PUBLIC METHODS
 
-    /**
-     * Creates a new article of the given type and opens that article in the editor
-     *
-     * @param {Event} $event
-     * @param {string} type
-     * @return {Promise.<Article>} Resolves after the article has been created and saved on the server
-     */
-    function createArticle($event, type) {
-      var dialog = $mdDialog.prompt()
-        .targetEvent($event)
-        .title('Create Page')
-        .textContent('Please provide a title for the new page.')
-        .placeholder('title')
-        .ok('Create')
-        .cancel('Cancel');
-
-      var titlePromise = $mdDialog.show(dialog);
-
-      titlePromise.then(function (title) {
-        var page = articlesRepo.create(type, title);
-        var savePromise = articlesRepo.save(page);
-
-        savePromise.then(function (page) {
-          $state.go('about.edit', { id: page.id });
-          sdaToast.success('Page created successfully');
-        }, function () {
-          sdaToast.error('Unable to create page');
-        });
-
-        return savePromise;
-      });
+    function editAboutPage(page) {
+      $state.go('about.edit', { id: page.id });
     }
   }
 
