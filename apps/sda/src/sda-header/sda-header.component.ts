3584a423c132b57c971a23741eccac7171e91ff7
Matthew Barry <mbarry@tamu.edu>
2017-02-17 14:12:10 -0600

    add binding to optionally show oxford logo in sda-header

--- apps/sda/src/sda-header/sda-header.component.ts
+++ apps/sda/src/sda-header/sda-header.component.ts
@@ -1,10 +1,13 @@
 import * as angular from 'angular';
 
+import './sda-header.scss';
+
 export const component: angular.IComponentOptions = {
   template: require('./sda-header.html'),
   bindings: {
     activeUrl: '@',
     mdSidenavId: '@',
+    showOxfordLogo: '='
   },
 
   /** @ngInject */
